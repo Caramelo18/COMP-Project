da6ea4424260cee9c23359983103b360dbe87c4d
Diogo Campos <d.diogocampos@gmail.com>
2017-04-06 23:55:17 +0100

    Using graphstream to parse files

---
+++ faops/ParseDot.java
@@ -0,0 +1,28 @@
+package faops;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSourceDOT;
+
+import java.io.IOException;
+
+public class ParseDot {
+    Graph graph;
+
+    public void parseFile(String path) {
+        graph = new DefaultGraph(path) ;
+        FileSourceDOT fs = new FileSourceDOT();
+
+        fs.addSink(graph);
+
+        try {
+        	fs.readAll(path);
+        } catch (IOException e) {
+        	e.printStackTrace();
+        } finally {
+        	fs.removeSink(graph);
+        }
+
+        System.out.println(graph.getEdgeCount());
+    }
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+javacc FAOPS.jj
+javac -cp "gs-core-1.3.jar:" *.java
+java -cp gs-core-1.3.jar: FAOPS
