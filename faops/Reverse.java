8599f8ed074481447d7bbb349f40264c881d6f4b
Caramelo18 <fabiocaramelo18@gmail.com>
2017-04-07 01:42:28 +0100

    changing edges direction on reverse

--- faops/Reverse.java
+++ faops/Reverse.java
@@ -3,21 +3,45 @@ package faops;
 import java.util.Iterator;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.graph.implementations.AbstractEdge;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
 
 public class Reverse {
 
-    private DefaultGraph reversedGraph;
+    private MultiGraph reversedGraph;
 
-    public Reverse(DefaultGraph graph) {
-        this.reversedGraph = graph;
-        Iterator iterator = graph.getEdgeIterator();
+    public Reverse(MultiGraph graph) {
+        this.reversedGraph = new MultiGraph(graph.getId() + "r");
 
+        Iterator<AbstractNode> nodeIterator = graph.getNodeIterator();
+        while(nodeIterator.hasNext())
+            this.reversedGraph.addNode(nodeIterator.next().getId());
+
+        Iterator<AbstractEdge> iterator = graph.getEdgeIterator();
+        int i = 5656;
         while(iterator.hasNext()){
-            System.out.println("From " + iterator.next().getNode0() + " to " + iterator.next().getNode1());
+            AbstractEdge e = iterator.next();
+            System.out.println("From " + e.getNode0() + " to " + e.getNode1());
+            String source = e.getNode0().getId();
+            String dest = e.getNode1().getId();
+            this.reversedGraph.addEdge(String.valueOf(i), dest, source);
+            i++;
         }
+
+
+        Iterator<AbstractEdge> iterator2 = reversedGraph.getEdgeIterator();
+        while(iterator2.hasNext()){
+            AbstractEdge e = iterator2.next();
+            System.out.println("New From " + e.getNode0() + " to " + e.getNode1());
+        }
+
+        DumpDot dump = new DumpDot(this.reversedGraph);
+        dump.dumpFile("exitTest.dot");
     }
 
-    public DefaultGraph getReversed() {
+    public MultiGraph getReversed() {
         return reversedGraph;
     }
 
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+rm *.class
+rm faops/*.class
 javacc FAOPS.jj
 javac -cp "gs-core-1.3.jar:" *.java
 java -cp gs-core-1.3.jar: FAOPS
