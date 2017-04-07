88a229a6f4661091c3b0a865479699ce23ff0ab9
Diogo Campos <d.diogocampos@gmail.com>
2017-04-07 02:41:14 +0100

    Using MultiGraph instead of DefaultGraph

--- faops/ParseDot.java
+++ faops/ParseDot.java
@@ -1,16 +1,16 @@
 package faops;
 
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.stream.file.FileSourceDOT;
 
 import java.io.IOException;
 
 public class ParseDot {
-    DefaultGraph graph;
+    Graph graph;
 
     public void parseFile(String path) {
-        graph = new DefaultGraph(path) ;
+        graph = new MultiGraph(path) ;
         FileSourceDOT fs = new FileSourceDOT();
 
         fs.addSink(graph);
