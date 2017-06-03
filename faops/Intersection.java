db0aba0d19f46de59cd146a73152210bc93521bb
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-06-03 22:03:24 +0100

    intersection implemented NOT TESTED !!!

--- faops/Intersection.java
+++ faops/Intersection.java
@@ -1,5 +1,38 @@
 package faops;
 
-public class Intersection {
+import java.util.Iterator;
+import org.graphstream.graph.*;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.*;
 
+public class Intersection {
+  private MultiGraph newGraph;
+
+  private MultiGraph graphA;
+  private MultiGraph graphB;
+
+  public Intersection(MultiGraph graphA, MultiGraph graphB)
+  {
+      this.graphA = graphA;
+      this.graphB = graphB;
+
+      newGraph = new MultiGraph("intersection");
+      intersection();
+  }
+
+  public MultiGraph getGraph()
+  {
+      return newGraph;
+  }
+
+  // source: http://stackoverflow.com/questions/4449950/how-do-you-construct-the-union-of-two-dfas
+  private void intersection(){
+      /*createNodes();
+      createEdges();*/
+
+      newGraph = union(complement(this.graphA),complement(this.graphB))
+
+      DumpDot dump = new DumpDot(newGraph);
+      dump.dumpFile("exitTest.dot");
+  }
 }
