531df080db023b8f08e40b4b16ab1fc94b963f91
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-06-03 01:28:58 +0100

    working in complement

--- faops/Complement.java
+++ faops/Complement.java
@@ -1,5 +1,104 @@
 package faops;
 
+import java.util.Iterator;
+import org.graphstream.graph.*;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.*;
+
 public class Complement {
+  private MultiGraph newGraph;
+
+  private MultiGraph initGraph;
+
+  public Complement(MultiGraph initGraph)
+  {
+      this.initGraph = initGraph;
+
+      newGraph = new MultiGraph("complement");
+      complement();
+  }
+
+  public MultiGraph getGraph(){
+      return newGraph;
+  }
+
+  public void complement(){
+    createNodes();
+    createEdges();
+
+    DumpDot dump = new DumpDot(newGraph);
+    dump.dumpFile("exitTest.dot");
+  }
+
+  private void createNodes(){
+      Iterator<AbstractNode> tempIterator = initGraph.getNodeIterator();
+
+      while(tempIterator.hasNext()){
+          AbstractNode tempNode = tempIterator.next();
+
+          String tempShape = tempNode.getAttribute("shape").toString();
+          String tempID = tempNode.getId();
+
+          if (tempShape.equals("circle")) {
+            MultiNode newNode = new MultiNode(this.newGraph, tempID);
+            newGraph.addNode(tempID);
+            newGraph.getNode(tempID).setAttribute("shape", "doublecircle");
+          } else if (tempShape.equals("doublecircle")) {
+            MultiNode newNode = new MultiNode(this.newGraph, tempID);
+            newGraph.addNode(tempID);
+            newGraph.getNode(tempID).setAttribute("shape", "circle");
+          }
+
+      }
+  }
+
+  private void createEdges(){
+      Iterator<AbstractEdge> tempIterator = initGraph.getEdgeIterator();
+
+      int id = 9871;
+      while(tempIterator.hasNext()){
+          AbstractEdge tempEdge = tempIterator.next();
+
+          if(!tempEdge.hasAttribute("label"))
+              continue;
+              
+          String tempID
+          String tempInput = tempEdge.getAttribute("label").toString();
+          String tempSource = tempEdge.getNode0().getId();
+          String tempDest = tempEdge.getNode1().getId();
+
+          }
+      }
+
+  }
+
+  //aux functions
+  private boolean isPoint(AbstractNode node){
+      if(node.hasAttribute("shape")){
+          String shape = node.getAttribute("shape").toString();
+          return shape.equals("point");
+      }
+      return false;
+  }
+
+  private boolean isFinal(AbstractNode node){
+      if(node.hasAttribute("shape")){
+          String shape = node.getAttribute("shape").toString();
+          return shape.equals("doublecircle");
+      }
+      return false;
+  }
+
+  private String getNodeType(AbstractNode tempNode, AbstractNode nodeB){
+      if(tempNode.hasAttribute("shape") && nodeB.hasAttribute("shape")){
+          String shapeA = tempNode.getAttribute("shape").toString();
+          String shapeB = nodeB.getAttribute("shape").toString();
+
+          if(shapeA.equals("doublecircle") && shapeB.equals("doublecircle"))
+              return "doublecircle";
+      }
+      return "circle";
+  }
+
 
 }
