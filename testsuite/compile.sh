dfab31fa35b44a7c93f54ba02fd18f81c6fd47b5
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-06-06 00:54:27 +0100

    removed some scripts from testsuite

--- testsuite/compile.sh
+++
@@ -1,6 +0,0 @@
-rm *.class
-rm faops/*.class
-jjtree FAOPS.jjt
-javacc FAOPS.jj
-javac -cp "gs-core-1.3.jar:" *.java */*.java
-java -cp gs-core-1.3.jar: FAOPS test.faops
--- testsuite/run.sh
+++
@@ -1,6 +0,0 @@
-rm *.class
-rm faops/*.class
-javacc FAOPS.jj
-javac -cp "gs-core-1.3.jar:" *.java
-java -cp gs-core-1.3.jar: FAOPS
-dot -Tpng exitTest.dot -o exitTest.png
